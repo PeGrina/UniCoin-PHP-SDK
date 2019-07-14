624fe3f485f1825ccaef174e3c8f265c4d139d51
PeGrina <46083846+PeGrina@users.noreply.github.com>
2019-07-14 12:16:14 +0500

    Update sdk.php

--- sdk.php
+++ sdk.php
@@ -1,9 +1,13 @@
 <?
 class UniCoinClient{
   private $merchant_id = 0;
+  private $curl = true;
   protected const API_HOST = "https://uc.simbrex.com/api/merchant/";
   private $key = "";
   public function __construct($merchant_id, $key){
+    if(version_compare('7.0.0', phpversion()) === 1) {
+      $this->curl = false;
+    }
     $this->merchant_id = $merchant_id;
     $this->key = $key;
   }
@@ -27,21 +31,29 @@ class UniCoinClient{
     $url = self::API_HOST.$method.".php";
     $params['merchant_id'] = $this->merchant_id;
     $params['key']= $this->key;
-    $content = http_build_query($params);
-    $content = $content.$this->key;
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        "Content-Type:multipart/form-data"
-    ));
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-    $result = json_decode(curl_exec($ch), True);
-    curl_close($ch);
+    if($this->curl && function_exists("curl_init")){
+      $ch = curl_init();
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+          "Content-Type:multipart/form-data"
+      ));
+      curl_setopt($ch, CURLOPT_URL, $url);
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+      $result = json_decode(curl_exec($ch), True);
+      curl_close($ch);
+    }else{
+      $result = json_decode(file_get_contents($url, true, stream_context_create(array(
+          'http' => array(
+              'method'  => 'POST',
+              'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+              'content' => http_build_query($params)
+          )
+      ))), true);
+    }
     if($result['status'] == "succes"){
       return $result;
     }else{
-      error_log("Error UniCoinClient with number ".$result['error']['code']." - ".$result['error']['description']."", 0);
+      error_log("UniCoinClient Error with number ".$result['error']['code']." - ".$result['error']['description']."", 0);
       return $result;
     }
   }
@@ -51,7 +63,7 @@ class UniCoinClient{
   }
   public function sendPayment($to_id, $sum, $code = false){
     $code = $code?$code:rand(-20000, 20000);
-    $result = $this->request("transfer", array('to_id' => $to_id,'sum' => $sum, 'code' => $code));
+    $result = $this->request("transfer", array('sum' => $sum, 'code' => $code,'to' => $to_id));
     return $result;
   }
   public function getHistory($count = 1000, $offset = 0){
